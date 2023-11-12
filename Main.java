e891f2275dd9bd710a2518f3020c2612b0d1afcc
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-11-12 17:00:51 +0200

    updated

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
+        User user = new User("asmaa", "123456", "asmaaiheikal@gmail.com", "01000000000");
     }
 }
--- User.java
+++ User.java
@@ -1,3 +1,44 @@
 public class User {
-    
+    private String userName;
+    private String password;
+    private String email;
+    private String mobileNumber;
+    //private InstapayAccount instapayAccount;
+
+    public User(String userName, String password, String email, String mobileNumber) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.mobileNumber = mobileNumber;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    //public InstapayAccount getInstapayAccount() {
+    //    return instapayAccount;
+    //}
+    //public void setInstapayAccount(InstapayAccount instapayAccount) {
+    //    this.instapayAccount = instapayAccount;
+    //}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
