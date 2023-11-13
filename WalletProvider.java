796e0188dcdf853abfb019994e65f8efaa830ed4
Laila Kandil <101605144+LailaKandil@users.noreply.github.com>
2023-11-13 23:05:15 +0200

    Update VodafoneCashProvider in WalletProvider.java

--- WalletProvider.java
+++ WalletProvider.java
@@ -1,20 +1,38 @@
 public interface WalletProvider {
     public boolean verifyWallet(String mobileNumber);
-    public double getBalance(double balance);
-    public boolean checkBalance(double balance);
+    public double getBalance(String mobileNumber);
+    public boolean checkBalance(String mobileNumber,double amount);
     public void withdrawal(double amount);
     public void deposit(double amount);
 
 }
-public abstract class VodafoneCashProvider implements WalletProvider{
+abstract class VodafoneCashProvider implements WalletProvider{
+    private VodafoneCash vodafoneCash;
+    @Override
+    public boolean verifyWallet(String mobileNumber){
+        return vodafoneCash.verifyWallet(mobileNumber);
+    }
+    @Override
+    public double getBalance(String mobileNumber){
+        return vodafoneCash.getBalance(mobileNumber);
+    }
+    @Override
+    public boolean checkBalance(String mobileNumber,double amount) {
+        return vodafoneCash.checkBalance(mobileNumber,amount);
+    }
+    public void withdrawal(double amount){
+        vodafoneCash.withdrawal(amount);
+    }
+    public void deposit(double amount){
+        vodafoneCash.deposit(amount);
+    }
+}
+
+abstract class CIBWalletProvider implements WalletProvider{
     //    implements all the functions in walletprovider but in its own unique way than the other wallet providers
 }
 
-public abstract class CIBWalletProvider implements WalletProvider{
-    //    implements all the functions in walletprovider but in its own unique way than the other wallet providers
-}
-
-public abstract class FawryProvider implements WalletProvider{
+abstract class FawryProvider implements WalletProvider{
 //    implements all the functions in walletprovider but in its own unique way than the other wallet providers
 }
 
