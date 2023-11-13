9fc196fe9f445fa64192a064c8c6a8c187914889
Laila Kandil <101605144+LailaKandil@users.noreply.github.com>
2023-11-13 15:35:26 +0200

    Update WalletProvider.java

--- WalletProvider.java
+++ WalletProvider.java
@@ -6,3 +6,15 @@ public interface WalletProvider {
     public void deposit(double amount);
 
 }
+public abstract class VodafoneCashProvider implements WalletProvider{
+    //    implements all the functions in walletprovider but in its own unique way than the other wallet providers
+}
+
+public abstract class CIBWalletProvider implements WalletProvider{
+    //    implements all the functions in walletprovider but in its own unique way than the other wallet providers
+}
+
+public abstract class FawryProvider implements WalletProvider{
+//    implements all the functions in walletprovider but in its own unique way than the other wallet providers
+}
+
