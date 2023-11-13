8dc73e9caf3689f2bd569041e97de0014d22892a
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2023-11-13 16:52:10 +0200

    finished register and login

--- OTP.java
+++ OTP.java
@@ -1,8 +1,10 @@
 import java.util.Properties;
 import java.util.Random;
+import java.util.Scanner;
 
 public class OTP {
 
+    private String otp;
     public String generateOTP() {
 
         // Characters allowed in the OTP
@@ -20,18 +22,21 @@ public class OTP {
             int index = random.nextInt(allowedChars.length());
             otp.append(allowedChars.charAt(index));
         }
+        this.otp = otp.toString();
         // Return the OTP as a string
         return otp.toString();
     }
 
-    public void sendOTP(String userEmail, String OTP) {
-       System.out.println("Your OTP is: " + generateOTP());
+    public void sendOTP() {
+        String otp = generateOTP();
+        System.out.println("Your OTP is: " + otp);
     }
 
     public boolean verifyOTP() {
         System.out.print("Enter the OTP you received: ");
-        String userOTP = System.console().readLine();
-        if (userOTP.equals(generateOTP())) {
+        Scanner scanner = new Scanner(System.in);
+        String userOTP = scanner.nextLine();
+        if (userOTP.equals(this.otp)) {
             System.out.println("OTP verified successfully!");
             return true;
         } else {
--- Storage.java
+++ Storage.java
@@ -1,5 +1,127 @@
+import java.io.*;
+import java.util.ArrayList;
+
+//Strategy Pattern
 public interface Storage<T>{
-    public boolean add(T object);
-    public boolean delete(String uniqueIdentifier);
-    public User get(String uniqueIdentifier);
+    public boolean add(T object) throws IOException;
+    public T read(String uniqueIdentifier) throws FileNotFoundException;
+    public ArrayList<T> readAll() throws FileNotFoundException;
+}
+
+
+class InMemoryStorage implements Storage<User> {
+    private ArrayList<User> users;
+
+    public InMemoryStorage() {
+        this.users = new ArrayList<>();
+    }
+
+    @Override
+    public boolean add(User user) {
+        for (User u : users) {
+            if (u.getUserName().equals(user.getUserName())) {
+                return false;
+            }
+        }
+        users.add(user);
+        return true;
+    }
+    @Override
+    public User read(String uniqueIdentifier) {
+        String username;
+        for (User user : users) {
+            username = user.getUserName();
+            if (username.equals(uniqueIdentifier)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+    @Override
+    public ArrayList<User> readAll() {
+        return users;
+    }
+    public boolean isMobileNumberExist(String mobileNumber) {
+        for (User user : readAll()) {
+            if (user.getMobileNumber().equals(mobileNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isUserNameExist(String userName) {
+        for (User user : readAll()) {
+            if (user.getUserName().equals(userName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+class FileStorage implements Storage<User>{
+    @Override
+    public boolean add(User user) throws IOException {
+        try {
+            FileWriter writer = new FileWriter("users.txt", true);
+            writer.write(user.getUserName() + " " + user.getPassword() + " " + user.getMobileNumber()+" "+ user.getTypeOfInstapayAccount() + "\n");
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    @Override
+    public User read(String uniqueIdentifier) throws FileNotFoundException {
+        FileReader reader = new FileReader("users.txt");
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(" ");
+                if (words[0].equals(uniqueIdentifier)) {
+                    User user = new User(words[0], words[1], words[2], words[3]);
+                    return user;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Override
+    public ArrayList<User> readAll() throws FileNotFoundException {
+        FileReader reader = new FileReader("users.txt");
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line;
+        ArrayList<User> users = new ArrayList<>();
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(" ");
+                User user = new User(words[0], words[1], words[2], words[3]);
+                users.add(user);
+            }
+            return users;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public boolean isMobileNumberExist(String mobileNumber) throws FileNotFoundException {
+        for (User user : readAll()) {
+            if (user.getMobileNumber().equals(mobileNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isUserNameExist(String userName) throws FileNotFoundException {
+        for (User user : readAll()) {
+            if (user.getUserName().equals(userName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
