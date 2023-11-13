f0023f9d851be12183072775efc0cb3147a50346
Laila Kandil <101605144+LailaKandil@users.noreply.github.com>
2023-11-13 15:26:53 +0200

    Removed the Wallet Provider classes and placed them inside WalletProvider

--- VodafoneCashProvider.java
+++
@@ -1,3 +0,0 @@
-public abstract class VodafoneCashProvider implements WalletProvider{
-    //    implements all the functions in walletprovider but in its own unique way than the other wallet providers
-}
