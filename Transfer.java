343b0de3a113d6f94fe9698bd6460ad77bd7c505
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2023-11-15 09:10:42 +0200

    updates

--- Transfer.java
+++ Transfer.java
@@ -6,19 +6,20 @@ public interface Transfer {
 class WalletTransfer implements Transfer {
 
     WalletProvider wallet;
+    @Override
     public void transferMoney(InstapayAccount s ,String recipient, double amount) {
         //sends the recipient the amount using phone number
+        if(s instanceof WalletAccount){
+            ((WalletAccount) s).wallet.withdrawal(amount);
+        }
     }
 }
 
 class BankTransfer implements Transfer {
 
-
+    @Override
     public void transferMoney(InstapayAccount s ,String recipient, double amount) {
         //transfers to the bank account (recipient) the amount using bank account number
-        if(s instanceof BankAccount){
-
-        }
 
     }
 }
@@ -28,5 +29,6 @@ class InstapayTransfer implements Transfer {
     @Override
     public void transferMoney(InstapayAccount s ,String recipient, double amount) {
         //transfers amount to another instapay account using username
+
     }
 }
